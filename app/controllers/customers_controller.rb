2c1ba6be055b5a5feb1ff409f969ed61749d8cca
Catherine Gloria <catherinegloria@gmail.com>
2021-01-22 16:39:44 -0800

    Signup Page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,25 @@
 class CustomersController < ApplicationController
   def new
+    @customer = Customer.new
   end
+
+  def create
+    "hello world"
+  end
+
+  def show
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :name,
+      :email,
+      :password,
+      :city,
+      :state
+    )
+  end
+
 end
