9724522a86c2842c35707adcd95e6e85f4edba2c
Catherine Gloria <catherinegloria@gmail.com>
2021-01-22 20:14:14 -0800

    Log in updates

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,7 +3,7 @@ class SessionController < ApplicationController
   end
 
   def create
-    if @customer = Customer.find_by(name: params[:customer][:name])
+    if @customer = Customer.find_by(email: params[:email])
       session[:user_id] = @customer.id
       redirect_to customer_path(@customer)
     else
@@ -12,7 +12,7 @@ class SessionController < ApplicationController
   end
 
   def destroy
-    session.delete :customer_id
+    session.clear
     redirect_to root_path
   end
 end
