2ae8e6e206b51f47e34c38a4b9de1aebbb4947c5
Catherine Gloria <catherinegloria@gmail.com>
2021-01-22 21:54:51 -0800

    Business controller and views progress

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,2 +1,26 @@
 class BusinessesController < ApplicationController
+
+  def index
+    @businesses = Business.all
+  end
+
+  def new
+    @business = Business.new
+  end
+
+  def create
+    business = category.businesses.build(business_params))
+    if business.save
+      redirect_to business_path(business)
+    else
+      render new_business
+    end
+  end
+
+  private
+
+  def business_params
+    params.require(:business).permit(:name, :phone_number, :city, :state, :price_range, :category_id)
+  end
+
 end
