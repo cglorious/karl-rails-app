33a2061520e915289a9a7a687a23b0d3e6fd0cef
Catherine Gloria <catherinegloria@gmail.com>
2021-02-09 15:44:33 -0800

    most reviewed business

--- app/models/review.rb
+++ app/models/review.rb
@@ -11,13 +11,8 @@ class Review < ApplicationRecord
     end
 
     @sorted = @array.sort
+    #iterate through array to see which element occurred the most 
     most = @sorted[0]
     business = Business.find_by_id(most)
-    #@sorted[-1]
-    #[1, 1, 2, 3, 1, 2, 4, 4, 1]
-    #check which value has the most
   end
 end
-
-#create a link
-#land on the business/:id
