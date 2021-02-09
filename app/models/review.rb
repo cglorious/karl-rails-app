6c06edd703e2c497e4fa2703811272a2f5d79954
Catherine Gloria <catherinegloria@gmail.com>
2021-02-09 15:43:18 -0800

    most reviewed business

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,22 @@ class Review < ApplicationRecord
   belongs_to :customer
   belongs_to :business
   validates :rating, presence: true
+
+  def self.most_reviewed_business
+    @array = []
+
+    self.all.each do |r|
+      @array << r.business_id
+    end
+
+    @sorted = @array.sort
+    most = @sorted[0]
+    business = Business.find_by_id(most)
+    #@sorted[-1]
+    #[1, 1, 2, 3, 1, 2, 4, 4, 1]
+    #check which value has the most
+  end
 end
+
+#create a link
+#land on the business/:id
